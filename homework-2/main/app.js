e88bd8813b9b0b01df7fa4c54a4b21f5e9898272
Stefania Eliza <busuiocstefania20@stud.ase.ro>
2022-11-16 17:20:53 +0200

    v2

--- homework-2/main/app.js
+++ homework-2/main/app.js
@@ -1,15 +1,18 @@
 function addTokens(input, tokens){
   let modified = input;
+  
   if(typeof modified !== 'string'){
     throw new Error ('Invalid input')
   }
   else if(modified.trim().length<6){
     throw new Error ('Input should have at least 6 characters')
   }
-  else if(typeof tokens[0].tokenName!=="string" && tokens[0]!=='{tokenName:'+tokens[0].tokenName+'}' ){
-    throw new Error("Invalid array format");
-  }
   else{
+    for (var i = 0; i < tokens.length; i++) {
+      if (typeof tokens[i].tokenName!=="string" && tokens[i]!=='{tokenName:'+tokens[i].tokenName+'}' ) {
+          throw new Error("Invalid array format");
+      }
+  }
   for(let i=0; i< tokens.length;i++){
   if (modified.indexOf("...") !== -1) {
        modified = modified.replace("...", '${'+ tokens[i].tokenName + '}');
